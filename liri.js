a7f51e17f18b36cc08cccde6ba852c8c5f0d7028
Mike M <mikal815@users.noreply.github.com>
2018-07-18 22:46:50 -0400

    initial setup

---
+++ liri.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+
+
+// * `my-tweets`
+
+// * `spotify-this-song`
+
+// * `movie-this`
+
+// * `do-what-it-says`
+
