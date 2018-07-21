7a324a223cb468408fd22ce6679e84e6fb175106
Mike M <mikal815@users.noreply.github.com>
2018-07-21 16:43:45 -0400

    got the 'do-what-it-says' function working

--- liri.js
+++ liri.js
@@ -9,7 +9,7 @@ var movieName = process.argv[3];
 var liriReturn = process.argv[2];
 var twitter = require('twitter');
 var client = new twitter(keys.twitter);
-
+var spotifySong = process.argv[3];
 
 
 // Switches
@@ -19,11 +19,15 @@ switch (liriReturn) {
     break;
 
     case "spotify-this-song":
-    spotifyThisSong();
+    spotifyThisSong(spotifySong);
     break;
     
     case "movie-this":
     movieThis();
+    break;
+
+    case "do-what-it-says":
+    doWhatItSays();
     break;
 
     default: console.log("\n" + "type one of the following commands after 'node liri.js': " + "\n" +
@@ -51,11 +55,16 @@ function myTweets() {
 };
 
 function spotifyThisSong(songName) {
-    var songName = process.argv[3];
-    if (!songName) {
-        songName = "The Sign";
+    console.log('Do what ' + songName);
+    // var songName = process.argv[3];
+    if (songName) {
+        var songRequest = songName;
+       ;
+    } else {
+        var songRequest = "The Sign Ace of Base"
+        // var songName = process.argv[3];
     };
-    songRequest = songName;
+    // 
     spotify.search({
         type: "track",
         query: songRequest
@@ -107,3 +116,42 @@ function movieThis() {
         };
     });
 };
+
+
+function doWhatItSays(){
+
+    var fs = require('fs');
+    
+      fs.readFile('random.txt','utf8', function(err,data){
+          if(err) throw err;
+    
+          var dataSplit = data.split(',');
+              userInput = dataSplit[0];
+              userChoice = dataSplit[1];
+    
+                console.log(dataSplit[0]);
+                console.log(dataSplit[1]);
+
+          switch(userInput){
+    
+              case "my-tweets":
+                myTweets();
+                break;
+    
+              case "spotify-this-song":
+                spotifyThisSong(userChoice);
+                break;
+            
+              case "movie-this":
+                movieThis();
+                break;
+
+              case "do-what-it-says":
+                doWhatItSays();
+                break;  
+  
+          }
+    
+      });
+    
+    };
