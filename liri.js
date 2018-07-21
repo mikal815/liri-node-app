cfcb3b4ed3d5f3574c6b027a5e5b3b9b67d199d9
Mike M <mikal815@users.noreply.github.com>
2018-07-20 22:32:10 -0400

    Got twitter api function working

--- liri.js
+++ liri.js
@@ -2,19 +2,35 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var fs = require("fs");
-var Spotify = require('node-spotity-api');
-var spotify = new Spotify(keys.spotify);
+// var Spotify = require('node-spotity-api');
+// var spotify = new Spotify(keys.spotify);
 var request = require("request")
+var liriReturn = process.argv[2];
 var twitter = require('twitter');
 var client = new twitter(keys.twitter);
 
 
 
-// * `my-tweets`
+// Switches
+switch (liriReturn) {
+    case "my-tweets":
+    myTweets();
+    break;
+};
 
-// * `spotify-this-song`
 
-// * `movie-this`
-
-// * `do-what-it-says`
+// Functions
+function myTweets() {
+    var params = {screen_name: 'chiller01549953'};
+    client.get('statuses/user_timeline', params, function (error, tweets, response){
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text);
+            };
+        } else {
+            console.log("error: " + err);
+            return
+        };
+    });
+};
 
