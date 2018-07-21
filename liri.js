ce09d3ca26a1da13466a1bf1265db9dc87c7b91c
Mike M <mikal815@users.noreply.github.com>
2018-07-20 21:17:49 -0400

    added variables

--- liri.js
+++ liri.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 
+var keys = require("./keys.js");
+var fs = require("fs");
+var Spotify = require('node-spotity-api');
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var request = require("request")
+var twitter = require('twitter');
+var client = new twitter(keys.twitter);
 
 
 
